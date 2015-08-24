1f60b96fb14ac38048f342c403d8e80210f998aa
Baguz Ach <eggoez@users.noreply.github.com>
2015-08-24 08:41:07 +0700

    Pebble time
    changed font on pebble time (dot)

---
+++ src/time_as_words.c
@@ -0,0 +1,80 @@
+#include "time_as_words.h"
+
+//jame
+static const char* const HOURS[] = {
+  "",
+	"siji",
+	"loro",
+	"telu",
+	"papat",
+	"limo",
+	"enem",
+	"pitu",
+	"wolu",
+	"songo",
+  "spuloh",
+  "swelas",
+  "rolas"
+};
+
+//menit
+static const char* const ONES[] = {
+  "",
+	"siji",
+	"loro",
+	"telu",
+	"papat",
+	"limo",
+	"enem",
+	"pitu",
+	"wolu",
+	"songo"
+};
+
+void time_as_words(int int_hour, int int_min, char* str_hour, char* str_tens, char* str_ones) {
+	
+	strcpy(str_hour, "");
+	strcpy(str_tens, "");
+	strcpy(str_ones, "");
+  
+	//jam
+  if (int_hour % 12 == 0) {
+		strcat(str_hour, HOURS[12]);
+	}
+	else {
+		strcat(str_hour, HOURS[int_hour % 12]);
+	}
+	
+	//menit
+  if(int_min == 0){
+    strcat(str_tens, "pas..");
+  }
+  else if (int_min <= 10){
+    if(int_min == 10){
+      strcat(str_tens, "spuloh");
+    }
+    else{
+      strcat(str_tens, ONES[int_min % 10]);
+    }
+  } 
+  //update nang versi 0.2 iki dibuak soale bentrok
+  /*
+	else if (int_min > 10 && int_min < 20) {
+    if(int_min == 11){
+      strcat(str_tens, "swelas");
+    }
+    else {
+      strcat(str_tens, ONES[int_min - 10]);
+      strcat(str_ones, "belas");
+    }	  
+	} */
+	else {    
+		strcat(str_tens, ONES[int_min / 10]);
+    if(int_min % 10 == 0){
+      strcat(str_ones, "enol");
+    }
+    else {
+      strcat(str_ones, ONES[int_min % 10]);
+    }
+	}
+}
